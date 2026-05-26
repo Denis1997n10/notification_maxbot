1b45fba9dc781515c19b3ba0e2ef9372692fb967
Denis1997n10 <45227449+Denis1997n10@users.noreply.github.com>
2026-05-26 16:36:00 +0400

    Fix Terraform S3 backend config for Yandex Object Storage

--- scripts/bootstrap_yc.sh
+++ scripts/bootstrap_yc.sh
@@ -63,10 +63,14 @@ cat > "infra/terraform/backend-${env}.hcl" <<HCL
 bucket = "${state_bucket}"
 key = "resident-notifications/${env}/terraform.tfstate"
 region = "ru-central1"
-endpoint = "https://storage.yandexcloud.net"
+endpoints = {
+  s3 = "https://storage.yandexcloud.net"
+}
 skip_region_validation = true
 skip_credentials_validation = true
 skip_metadata_api_check = true
+skip_requesting_account_id = true
+use_path_style = true
 HCL
 
 cat > ".local/${env}/backend.env" <<ENV
